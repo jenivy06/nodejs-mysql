30346dd1aaba9f0a8236f7e657ce6d585ee80c39
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-06-09 22:58:06 -0600

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# nodejs-mysql
+Bamazon!
+
+###### About this app:
+
+Users are presented with a list of items for sale and their price. Using their arrow key, they can select the item's ID number. Then they'll be asked how many they want to buy. If there is enough quantity of inventory, their purchase will be completed.
+
+This app was created using Node.js, MySQL, and the Inquirer NPM package. See screenshots below:
+
+![sequel](screenshots/sequel.png)
+
+![intial](screenshots/intiial.png)
+
+![sequel](screenshots/howmany.png)
+
+![sequel](screenshots/thankyou.png)
+
+![sequel](screenshots/mysql.png)
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -43,6 +43,9 @@ function Product(item_id, stock_quantity) {
   
      newProduct.printInfo();
   })
+
+  // function that will prompt questions for customer and check to see if there is enough quanity
+  
   
     inquirer
     .prompt([
@@ -50,17 +53,26 @@ function Product(item_id, stock_quantity) {
       {
         type: 'list',
         name: 'productid',
-        message: 'What is the item_id of the product you want',
+        message: 'What is the Item ID of the product you want?',
         choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'],
       }, {
         
         name: 'units',
         message: 'How many do you want to buy?',
         
-      }
+    
+  }, {
+        
+    name: 'thank you',
+    message: 'Thank you, your order has been placed!',
+    
+}
     ])
     .then(answers => {
       console.info('Answer:', answers.productid);
     });
    
+    // Check number of units to see if there are enough
     
+    
+                                                   
